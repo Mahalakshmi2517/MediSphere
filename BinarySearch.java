2743a82a55ab0c2331cf3ec1cf8173f1f75a7d52
Mahalakshmi K <mahalakshmi.k20067@gmail.com>
2026-07-01 21:25:25 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        int index = Arrays.binarySearch(arr, 40);
+
+        System.out.println("Index = " + index);
+    }
+}
